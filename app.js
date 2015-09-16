d3683627f6833935f637b39a846e22dc01ce7636
Charles Yang <charles.y.yang@oracle.com>
2015-09-16 14:52:38 -0400

    Add semi-colons and update about me

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
 /*
  * Module dependencies
  */
-var express = require('express')
-  , nib = require('nib')
-  
- var app = express()
+var express = require('express'),
+    nib = require('nib');
+
+var app = express();
+
 function compile(str, path) {
   return str
     .set('filename', path)
-    .use(nib())
+    .use(nib());
 }
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jade')
-app.use(express.logger('dev'))
 
-app.use(express.static(__dirname + '/public'))
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.render('index',
-  { title : 'Home' }
-  )
-})
+  res.render('index', { title : 'Home' });
+});
 
 app.get('/about', function (req, res) {
-  res.render('about',
-  { title : 'About' }
-  )
-})
+  res.render('about', { title : 'About' });
+});
 
-app.listen(process.env.PORT || 5000)
+app.listen(process.env.PORT || 5000);
